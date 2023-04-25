00ff4d241a213315d7e90d4fab24097d70016526
halkhadhami <khadh20@gmail.com>
2023-04-25 04:33:45 -0700

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       mail($to, $subject, $message, $headers);
 
       // Redirect to thank-you page
-      header("Location: thank-you.html");
+      header("https://halkhadhami.github.io/#contact");
       exit();
    }
 }
